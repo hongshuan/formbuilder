e305df18beef919f0b01d99081123947282642b9
Hanson Li <hanson@dealtap.ca>
2016-04-19 14:49:06 -0400

    Comment out finished forms

--- images.js
+++ images.js
@@ -2,16 +2,9 @@ var IMAGE_PATH = 'import/source-forms/';
 var IMAGE_PATH = 'file:///src/code/app-core/import/source-forms/gif/flat/';
 
 var images = [
-	"OREA Form 600 [2016]",
-	"OREA Form 651 [2016]",
-	"OREA Form 650 [2016]",
-	"OREA Form 560 [2016]",
-
-	"",
-	"OREA Form 632 [2016]-0",
-	"OREA Form 632 [2016]-1",
-	"OREA Form 632 [2016]-2",
-	"OREA Form 632 [2016]-3",
+	"OREA Form 545 [2016]-0",
+	"OREA Form 545 [2016]-1",
+	"OREA Form 545 [2016]-2",
 
 	"",
 	"OREA Form 601 [2016]-0",
@@ -20,8 +13,10 @@ var images = [
 	"OREA Form 601 [2016]-3",
 
 	"",
-	"OREA Form 565 [2016]-0",
-	"OREA Form 565 [2016]-1",
+	"OREA Form 632 [2016]-0",
+	"OREA Form 632 [2016]-1",
+	"OREA Form 632 [2016]-2",
+	"OREA Form 632 [2016]-3",
 
 	"",
 	"OREA Form 632 [2016]-0",
@@ -30,16 +25,22 @@ var images = [
 	"OREA Form 632 [2016]-3",
 
 	"",
-	"OREA Form 545 [2016]-0",
-	"OREA Form 545 [2016]-1",
-	"OREA Form 545 [2016]-2",
-
-	"",
 	"OREA Form 633 [2015]-0",
 	"OREA Form 633 [2015]-1",
 	"OREA Form 633 [2015]-2",
 	"OREA Form 633 [2015]-3",
 	"OREA Form 633 [2015]-4",
+/*
+	"",
+	"OREA Form 560 [2016]",
+	"OREA Form 600 [2016]",
+	"OREA Form 650 [2016]",
+	"OREA Form 651 [2016]",
+
+	"",
+	"OREA Form 565 [2016]-0",
+	"OREA Form 565 [2016]-1",
+*/
 ];
 
 function imageUrl(img) {
