30c5f211c37c967983a4a1f7fc7133c002985cf4
Hanson Li <hanson@dealtap.ca>
2016-04-19 16:43:09 -0400

    Add more forms

--- images.js
+++ images.js
@@ -2,35 +2,49 @@ var IMAGE_PATH = 'import/source-forms/';
 var IMAGE_PATH = 'file:///src/code/app-core/import/source-forms/gif/flat/';
 
 var images = [
+	"OREA Form 541 [2016]",
+	"",
+	"OREA Form 543 [2016]",
+	"",
+	"OREA Form 642 [2016]",
+	"",
 	"OREA Form 601 [2016]-0",
 	"OREA Form 601 [2016]-1",
 	"OREA Form 601 [2016]-2",
 	"OREA Form 601 [2016]-3",
-
 	"",
 	"OREA Form 632 [2016]-0",
 	"OREA Form 632 [2016]-1",
 	"OREA Form 632 [2016]-2",
 	"OREA Form 632 [2016]-3",
-
 	"",
 	"OREA Form 633 [2015]-0",
 	"OREA Form 633 [2015]-1",
 	"OREA Form 633 [2015]-2",
 	"OREA Form 633 [2015]-3",
 	"OREA Form 633 [2015]-4",
-/*
 	"",
+	"OREA Form 540 [2016]-0",
+	"OREA Form 540 [2016]-1",
+	"OREA Form 540 [2016]-2",
+	"",
+	"OREA Form 639 [2015]-0",
+	"OREA Form 639 [2015]-1",
+	"OREA Form 639 [2015]-2",
+	"OREA Form 639 [2015]-3",
+	"",
+	"OREA Form 660 [2016]-0",
+	"OREA Form 660 [2016]-1",
+
+/*
 	"OREA Form 560 [2016]",
 	"OREA Form 600 [2016]",
 	"OREA Form 650 [2016]",
 	"OREA Form 651 [2016]",
 
-	"",
 	"OREA Form 565 [2016]-0",
 	"OREA Form 565 [2016]-1",
 
-	"",
 	"OREA Form 545 [2016]-0",
 	"OREA Form 545 [2016]-1",
 	"OREA Form 545 [2016]-2",
