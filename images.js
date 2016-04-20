844f86ece4f3353aad9bb9a8253e813c30cd4fc7
Hanson Li <hanson@dealtap.ca>
2016-04-20 12:48:26 -0400

    Forms

--- images.js
+++ images.js
@@ -2,12 +2,6 @@ var IMAGE_PATH = 'import/source-forms/';
 var IMAGE_PATH = 'file:///src/code/app-core/import/source-forms/gif/flat/';
 
 var images = [
-	"OREA Form 633 [2015]-0",
-	"OREA Form 633 [2015]-1",
-	"OREA Form 633 [2015]-2",
-	"OREA Form 633 [2015]-3",
-	"OREA Form 633 [2015]-4",
-	"",
 	"OREA Form 660 [2016]-0", // checkbox/signature
 	"OREA Form 660 [2016]-1",
 	"",
@@ -31,6 +25,12 @@ var images = [
 	"OREA Form 639 [2015]-3",
 
 /*
+	"OREA Form 633 [2015]-0",
+	"OREA Form 633 [2015]-1",
+	"OREA Form 633 [2015]-2",
+	"OREA Form 633 [2015]-3",
+	"OREA Form 633 [2015]-4",
+	"",
 	"OREA Form 560 [2016]",
 	"OREA Form 600 [2016]",
 	"OREA Form 650 [2016]",
